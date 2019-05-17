fe7cb9c55f155f9079e8351ba36c910ac4e3544e
apeterescam1982 <alekkaiep@gmail.com>
2019-05-17 15:20:49 -0500

    Test commit

---
+++ src/car_sprit.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class car_sprit extends Applet {
+    public void paint(Graphics g){
+        Expo.fillRectangle(g,100,100,125,150);
+        Expo.fillRectangle(g,105,110,95,120);
+    }
+}
