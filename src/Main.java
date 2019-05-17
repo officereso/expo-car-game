ff96741619b52335da51489d09cf31ba1037af2d
apeterescam1982 <alekkaiep@gmail.com>
2019-05-17 15:08:45 -0500

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
-public class Main {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Main extends Applet {
+    public void paint(Graphics g){
+        Expo.fillRectangle(g,1,1,154,15);
+    }
 }
