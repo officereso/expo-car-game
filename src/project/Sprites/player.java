3136e334bb6dd6316416a9409fd99f53e6ca5bb4
officereso <qrs01@snowtx.com>
2019-05-20 10:53:13 -0500

    Tests created
    Signed-off-by: officereso <qrs01@snowtx.com>

--- src/project/Sprites/player.java
+++ src/project/Sprites/player.java
@@ -5,7 +5,7 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class player extends Applet {
-    public static void make(Graphics g){
+    public static void draw(Graphics g, int x1, int y1){
         Expo.fillRectangle(g,100,100,125,150);
         Expo.fillRectangle(g,105,110,95,120);
     }
---
+++ src/project/tests/movementTest.java
@@ -0,0 +1,5 @@
+package project.tests;
+
+public class movementTest {
+
+}
