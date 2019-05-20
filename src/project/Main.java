ea02e79a563b9790a9da6583fa38b66e260c5559
apeterescam1982 <alekkaiep@gmail.com>
2019-05-20 15:20:48 -0500

    Test commit

--- src/project/Main.java
+++ src/project/Main.java
@@ -7,9 +7,9 @@ import java.awt.*;
 public class Main extends Applet {
     public void paint(Graphics g){
         Expo.drawHeading(g, "Quentin Snow and Alekkai", "Final Project");
-        Draw.startScreen(g);
-        // Draw.road(g);
-        // project.Sprites.player.make(g);
+        //Draw.startScreen(g);
+        //Draw.road(g);
+        project.Sprites.player.make(g);
 
     }
 }
--- src/project/Sprites/colorsTemp.java
+++ src/project/Sprites/colorsTemp.java
@@ -1,12 +1,13 @@
-package project;
+package project.Sprites;
 
 import java.awt.*;
 
-public class Colors {
+public class colorsTemp {
     static final Color roadGrey = new Color(68,68,68);
     static final Color lineWhite = new Color(235, 235, 235);
     static final Color lineYellow = new Color(218, 190, 93);
     static final Color startGreen = new Color(13, 255, 0);
     static final Color startBlue = new Color(112, 113, 255);
     static final Color playerBody = new Color(247, 17, 17);
+    static final Color Tires = new Color(0, 0, 0);
 }
