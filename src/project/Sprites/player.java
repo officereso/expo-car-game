322bc0c97b057a8b9a5b6652778be6f4bac08dc0
apeterescam1982 <alekkaiep@gmail.com>
2019-05-21 14:53:04 -0500

    Usable player sprite

--- src/project/Sprites/player.java
+++ src/project/Sprites/player.java
@@ -10,9 +10,9 @@ public class player extends Applet {
         Expo.setColor(g,colorsTemp.playerBody);
         Expo.fillRectangle(g, 100, 100, 135, 160);
         Expo.setColor(g,colorsTemp.Tires);
-        Expo.fillRectangle(g, 105, 108, 90, 128);
-        Expo.fillRectangle(g, 105, 108, 90, 128);
-
-
-    }
+        Expo.fillRectangle(g, 105, 108, 95, 128);
+        Expo.fillRectangle(g, 105, 135, 95, 155);
+        Expo.fillRectangle(g, 130, 108, 140, 128);
+        Expo.fillRectangle(g, 130, 135, 140, 155);
+}
 }
