7fba7e16e2da696bfd787c989b4368664c761e95
officereso <qrs01@snowtx.com>
2019-05-24 21:23:35 -0500

    Code cleanup
    Signed-off-by: officereso <qrs01@snowtx.com>

--- src/project/Sprites/NPC3.java
+++ src/project/Sprites/NPC3.java
@@ -37,17 +37,3 @@ public class NPC3 extends Applet {
         }
     }
 }
-
-/*
-    public static void make(Graphics g){
-        int x = Expo.random(100,400);
-        Expo.setColor(g,SpriteColors.NPC1Body);
-        Expo.fillRectangle(g, x, 110, x+35, 170);
-        Expo.setColor(g,SpriteColors.Tires);
-        Expo.fillRectangle(g, x+5, 118, x-5, 138);
-        Expo.fillRectangle(g, x+5, 145, x-5, 165);
-        Expo.fillRectangle(g, x+30, 118, x+40, 138);
-        Expo.fillRectangle(g, x+30, 145, x+40, 165);
-
-    }
-}*/
